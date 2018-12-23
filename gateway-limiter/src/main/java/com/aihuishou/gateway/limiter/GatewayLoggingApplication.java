04789b5b86f7e54183e377e7b43e26357a751ed3
xiejiashuai <707094980@qq.com>
2018-12-23 20:32:42 +0800

    cloud gateway 升级成G版本

--- gateway-limiter/src/main/java/com/aihuishou/gateway/limiter/GatewayLoggingApplication.java
+++ gateway-limiter/src/main/java/com/aihuishou/gateway/limiter/GatewayLoggingApplication.java
@@ -1,4 +1,4 @@
-package com.aihuishou.gateway.logging;
+package com.aihuishou.gateway.limiter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
